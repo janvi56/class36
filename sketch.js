e2b9cacc18eb23e1ac8b70f6d70cb7d593c80c4a
janvi56 <77707554+janvi56@users.noreply.github.com>
2021-05-07 21:50:05 +0530

    car racing

---
+++ sketch.js
@@ -0,0 +1,17 @@
+var database;
+var gameState=0;
+var playerCount=0;
+var form,game,player;
+
+function setup(){
+createCanvas(400,400);
+database=firebase.database()
+
+game=new Game();
+game.getState();
+game.start();
+
+}
+function draw(){
+    
+}
